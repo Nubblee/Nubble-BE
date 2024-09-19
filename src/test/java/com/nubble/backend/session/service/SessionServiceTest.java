93069ab9aee632358b730f7db8967b02fe4d5c1c
biddan606 <biddan606@gmail.com>
2024-09-19 15:20:50 +0900

    test: sessionService.createSession() 성공 테스트 코드 작성

---
+++ src/test/java/com/nubble/backend/session/service/SessionServiceTest.java
@@ -0,0 +1,51 @@
+package com.nubble.backend.session.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.nubble.backend.session.service.SessionCommand.SessionCreateCommand;
+import com.nubble.backend.session.service.SessionInfo.SessionCreateInfo;
+import com.nubble.backend.user.domain.User;
+import com.nubble.backend.user.repository.UserRepository;
+import java.util.UUID;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionServiceTest {
+
+    @Autowired
+    private SessionService sessionService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @DisplayName("username, password와 매핑되는 User가 있다면, 새로운 세션을 발급합니다.")
+    @Test
+    void createSession_success() {
+        // given
+        User user = User.builder()
+                .username("user")
+                .password("1234")
+                .build();
+        userRepository.save(user);
+
+        SessionCommand.SessionCreateCommand command = SessionCreateCommand.builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .build();
+
+        String newSessionId = UUID.randomUUID().toString();
+
+        // when
+        SessionCreateInfo actual = sessionService.createSession(command);
+
+        // then
+        Assertions.assertAll(
+                () -> assertThat(actual.userId()).isEqualTo(user.getId()),
+                () -> assertThat(actual.sessionId()).isEqualTo(newSessionId)
+        );
+    }
+}
