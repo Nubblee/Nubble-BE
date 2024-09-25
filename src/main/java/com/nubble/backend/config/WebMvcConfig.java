621e681e0bb0f64aa0c9eadc2c60fcbe0471decd
biddan606 <biddan606@gmail.com>
2024-09-25 19:28:03 +0900

    feat: UserSession ArgumentResolver 구현

--- src/main/java/com/nubble/backend/config/WebMvcConfig.java
+++ src/main/java/com/nubble/backend/config/WebMvcConfig.java
@@ -1,9 +1,13 @@
 package com.nubble.backend.config;
 
+import com.nubble.backend.config.resolver.UserSessionResolver;
 import com.nubble.backend.interceptor.session.SessionCheckInterceptor;
+import com.nubble.backend.session.service.SessionRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,6 +16,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    private final SessionRepository sessionRepository;
     private final SessionCheckInterceptor sessionCheckInterceptor;
 
     @Bean
@@ -32,5 +37,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(sessionCheckInterceptor)
                 .addPathPatterns("/**");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new UserSessionResolver(sessionRepository));
     }
 }
--- src/main/java/com/nubble/backend/user/controller/UserApiController.java
+++ src/main/java/com/nubble/backend/user/controller/UserApiController.java
@@ -1,8 +1,10 @@
 package com.nubble.backend.user.controller;
 
-import static com.nubble.backend.user.controller.UserResponse.*;
+import static com.nubble.backend.user.controller.UserResponse.LoggedInUserGetResponse;
 
+import com.nubble.backend.config.resolver.UserSession;
 import com.nubble.backend.interceptor.session.SessionRequired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserApiController {
 
     @SessionRequired
     @GetMapping("/me")
-    public ResponseEntity<LoggedInUserGetResponse> getLoggedInUser() {
+    public ResponseEntity<LoggedInUserGetResponse> getLoggedInUser(UserSession userSession) {
         return ResponseEntity.ok()
                 .body(LoggedInUserGetResponse.builder()
                         .username("user")
