57b4c6070560c48c169896b45ac4e56eea6465c8
biddan606 <biddan606@gmail.com>
2024-09-25 23:06:37 +0900

    chore: 팀원 각각의 임시 아이디 생성

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,6 @@
 INSERT INTO users (username, password, nickname)
-VALUES ('test', '1234', '성오수민지영');
+VALUES ('test', '1234', '성오수민지영'),
+       ('ssuminii', '990430', '김수민'),
+       ('jizerozz', '051127', '박지영'),
+       ('sonseongoh', '960123', '손성오'),
+       ('biddan606', '960606', '유원우');
