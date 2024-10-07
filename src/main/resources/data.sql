54b9acd84e9921e94528452ce9a7fee66a08d249
biddan606 <biddan606@gmail.com>
2024-10-07 17:00:58 +0900

    chore: 코딩 문제 임시 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,3 +4,7 @@ VALUES ('test', '1234', '성오수민지영'),
        ('jizerozz', '051127', '박지영'),
        ('sonseongoh', '960123', '손성오'),
        ('biddan606', '960606', '유원우');
+
+INSERT INTO coding_problems (quiz_date, user_id, title, url)
+VALUES ('2024-10-06', 2, 'Lv0. 문자열의 뒤의 n글자', 'https://school.programmers.co.kr/learn/courses/30/lessons/181910'),
+       ('2024-10-05', 2, 'Lv1. 핸드폰 번호 가리기', 'https://school.programmers.co.kr/learn/courses/30/lessons/12948');
