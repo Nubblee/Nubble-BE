3016ca3d7437b7376c7897b58ba55acc53d25ca1
biddan606 <biddan606@gmail.com>
2024-10-17 18:51:52 +0900

    test: PostAssert 클래스를 활용한 임시 게시글 생성 테스트 개선

--- src/test/java/com/nubble/backend/post/service/PostServiceTest.java
+++ src/test/java/com/nubble/backend/post/service/PostServiceTest.java
@@ -6,11 +6,13 @@ import com.nubble.backend.board.domain.Board;
 import com.nubble.backend.board.service.BoardRepository;
 import com.nubble.backend.category.domain.Category;
 import com.nubble.backend.category.service.CategoryRepository;
+import com.nubble.backend.customassert.PostAssert;
 import com.nubble.backend.fixture.UserFixture;
 import com.nubble.backend.post.domain.Post;
 import com.nubble.backend.post.domain.PostStatus;
 import com.nubble.backend.post.service.PostCommand.PostCreateCommand;
 import com.nubble.backend.post.service.PostCommand.PostPublishCommand;
+import com.nubble.backend.post.shared.PostStatusDto;
 import com.nubble.backend.user.domain.User;
 import com.nubble.backend.user.service.UserRepository;
 import java.util.Optional;
@@ -42,8 +44,11 @@ class PostServiceTest {
     @Autowired
     private BoardRepository boardRepository;
 
+    private User user;
+
     @BeforeEach
     void setup() {
+        // 게시글이 속해있는 카테고리와 게시판을 생성한다.
         Category category = Category.builder()
                 .name("루트 카테고리")
                 .build();
@@ -54,20 +59,22 @@ class PostServiceTest {
                 .name("게시판 이름")
                 .build();
         boardRepository.save(board);
+
+        // 게시글을 작성할 유저를 생성한다.
+        user = UserFixture.aUser().build();
+        userRepository.save(user);
     }
 
-    @DisplayName("게시글을 생성한다.")
+    @DisplayName("임시 상태의 게시글을 생성한다.")
     @Test
-    void createPost_success() {
-        User user = UserFixture.aUser().build();
-        userRepository.save(user);
-
+    void createPost_shouldCreatePostWithDraftStatus() {
         // given
         PostCreateCommand command = PostCreateCommand.builder()
                 .title("제목입니다.")
                 .content("내용입니다.")
                 .userId(user.getId())
                 .boardId(board.getId())
+                .status(PostStatusDto.DRAFT)
                 .build();
 
         // when
@@ -77,22 +84,20 @@ class PostServiceTest {
         Optional<Post> postOptional = postRepository.findById(newPostId);
 
         assertThat(postOptional).isPresent();
-        assertThat(postOptional.get().getId()).isEqualTo(newPostId);
-        assertThat(postOptional.get().getTitle()).isEqualTo(command.title());
-        assertThat(postOptional.get().getContent()).isEqualTo(command.content());
-        assertThat(postOptional.get().getUser().getId()).isEqualTo(user.getId());
-        assertThat(postOptional.get().getStatus()).isEqualTo(PostStatus.DRAFT);
-        assertThat(postOptional.get().getThumbnailUrl()).isNull();
-        assertThat(postOptional.get().getDescription()).isNull();
+        PostAssert.assertThat(postOptional.get())
+                .hasId(newPostId)
+                .hasTitle(command.title())
+                .hasContent(command.content())
+                .hasUserId(user.getId())
+                .hasStatus(PostStatus.valueOf(command.status().name()))
+                .hasThumbnailUrl(null)
+                .hasDescription(null);
     }
 
     @DisplayName("게시글의 주인이 게시글을 게시합니다.")
     @Test
     void test() {
         // given
-        User user = UserFixture.aUser().build();
-        userRepository.save(user);
-
         PostCreateCommand postCreateCommand = PostCreateCommand.builder()
                 .title("제목입니다.")
                 .content("내용입니다.")
