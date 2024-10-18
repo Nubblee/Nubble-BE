ba05381086a892dbc2a66e223dfa338018b19fb5
biddan606 <biddan606@gmail.com>
2024-10-18 15:42:08 +0900

    feat: Post 도메인 필드의 제한사항을 명확하게 설정

--- src/main/java/com/nubble/backend/post/domain/Post.java
+++ src/main/java/com/nubble/backend/post/domain/Post.java
@@ -12,6 +12,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import java.util.Objects;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,23 +23,30 @@ import lombok.NoArgsConstructor;
 @Getter
 public class Post {
 
+    private static final int MAX_TITLE_LENGTH = 100;
+    private static final int MAX_CONTENT_LENGTH = 10_000;
+    private static final int MAX_THUMBNAIL_URL_LENGTH = 255;
+    private static final int MAX_DESCRIPTION_LENGTH = 255;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "post_id")
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = MAX_TITLE_LENGTH)
     private String title;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = MAX_CONTENT_LENGTH)
     private String content;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false, updatable = false)
     private User user;
 
+    @Column(length = MAX_THUMBNAIL_URL_LENGTH)
     private String thumbnailUrl;
 
+    @Column(length = MAX_DESCRIPTION_LENGTH)
     private String description;
 
     @Enumerated(value = EnumType.STRING)
@@ -57,43 +65,92 @@ public class Post {
             String description,
             PostStatus status,
             Board board) {
-        this.title = title;
-        this.content = content;
         this.user = user;
+        validateUser();
+
+        update(title, content, thumbnailUrl, description, status, board);
+    }
+
+    private void validateUser() {
+        if (user == null) {
+            throw new RuntimeException("유저는 필수입니다.");
+        }
+        if (user.getId() == null) {
+            throw new RuntimeException("저장된 유저만 참조할 수 있습니다.");
+        }
+    }
+
+    public void update(
+            String title,
+            String content,
+            String thumbnailUrl,
+            String description,
+            PostStatus status,
+            Board board) {
+        updateTitle(title);
+        updateContent(content);
+        updateThumbnailUrl(thumbnailUrl);
+        updateDescription(description);
+        updateStatus(status);
+        updateBoard(board);
+    }
+
+    private void updateTitle(String title) {
+        if (title == null || title.isEmpty()) {
+            throw new RuntimeException("제목은 비어있을 수 없습니다.");
+        }
+        if (title.length() > MAX_TITLE_LENGTH) {
+            throw new RuntimeException("제목은 최대 %d글자까지 가능합니다.".formatted(MAX_TITLE_LENGTH));
+        }
+        this.title = title;
+    }
+
+    private void updateContent(String content) {
+        if (content == null || content.isEmpty()) {
+            throw new RuntimeException("내용은 비어있을 수 없습니다.");
+        }
+        if (content.length() > MAX_CONTENT_LENGTH) {
+            throw new RuntimeException("내용은 최대 %d글자까지 가능합니다.".formatted(MAX_CONTENT_LENGTH));
+        }
+        this.content = content;
+    }
+
+    // todo url만 오도록 수정, 업로드한 url 링크만 올 수 있도록 수정
+    private void updateThumbnailUrl(String thumbnailUrl) {
+        if (Objects.nonNull(thumbnailUrl) && thumbnailUrl.length() > MAX_THUMBNAIL_URL_LENGTH) {
+            throw new RuntimeException("썸네일url은 최대 %d길이까지 가능합니다.".formatted(MAX_THUMBNAIL_URL_LENGTH));
+        }
         this.thumbnailUrl = thumbnailUrl;
+    }
+
+    private void updateDescription(String description) {
+        if (Objects.nonNull(description) && description.length() > MAX_DESCRIPTION_LENGTH) {
+            throw new RuntimeException("요약은 최대 %d글자까지 가능합니다.".formatted(MAX_DESCRIPTION_LENGTH));
+        }
         this.description = description;
-        this.status = status;
+    }
+
+    private void updateBoard(Board board) {
+        if (board == null) {
+            throw new RuntimeException("게시판은 필수입니다.");
+        }
+        if (board.getId() == null) {
+            throw new RuntimeException("저장된 게시판만 참조할 수 있습니다.");
+        }
         this.board = board;
-
-        if (this.status == PostStatus.PUBLISHED) {
-            if (thumbnailUrl == null || thumbnailUrl.isEmpty()) {
-                throw new RuntimeException("게시한 상태에서는 썸네일 URL이 필요합니다.");
-            }
-            if (description == null || description.isEmpty()) {
-                throw new RuntimeException("게시한 상태에서는 설명이 필요합니다.");
-            }
-        }
     }
 
-    public void publish() {
+    private void updateStatus(PostStatus status) {
         if (status == PostStatus.PUBLISHED) {
-            throw new RuntimeException("이미 게시된 상태입니다.");
+            if (thumbnailUrl == null) {
+                throw new RuntimeException("게시글을 게시하기 위해서는 썸네일이 필요합니다.");
+            }
+            if (description == null) {
+                throw new RuntimeException("게시글을 게시하기 위해서는 요약이 필요합니다.");
+            }
         }
-        status = PostStatus.PUBLISHED;
-    }
 
-    public void updateThumbnailUrl(String thumbnailUrl) {
-        if (thumbnailUrl == null) {
-            throw new RuntimeException("썸네일 값은 필수입니다.");
-        }
-        this.thumbnailUrl = thumbnailUrl;
-    }
-
-    public void updateDescription(String description) {
-        if (description == null) {
-            throw new RuntimeException("게시글 설명은 필수입니다.");
-        }
-        this.description = description;
+        this.status = status;
     }
 
     public void validateOwner(Long userId) {
