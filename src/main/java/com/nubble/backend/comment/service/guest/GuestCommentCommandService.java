9853cdf1d15d5e339547a549c60177c7c8eb35e3
biddan606 <biddan606@gmail.com>
2024-10-21 19:14:07 +0900

    feat: 게스트 댓글 저장 기능 구현

--- src/main/java/com/nubble/backend/comment/service/guest/GuestCommentCommandService.java
+++ src/main/java/com/nubble/backend/comment/service/guest/GuestCommentCommandService.java
@@ -1,20 +1,36 @@
 package com.nubble.backend.comment.service.guest;
 
+import com.nubble.backend.comment.domain.GuestComment;
 import com.nubble.backend.comment.service.CommentQuery.PostByIdQuery;
 import com.nubble.backend.comment.service.guest.GuestCommentCommand.CreateCommand;
+import com.nubble.backend.post.domain.Post;
+import com.nubble.backend.post.service.PostRepository;
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class GuestCommentCommandService {
+
+    private final PostRepository postRepository;
+    private final GuestCommentRepository guestCommentRepository;
 
     @Transactional
     public long create(
             PostByIdQuery postQuery,
             CreateCommand createCommand) {
-        // 게스트 댓글을 생성한다.
-        // 게시글과 댓글을 매핑한다.
-        // 댓글을 저장한다.
-        return 0;
+        Post post = postRepository.findById(postQuery.id())
+                .orElseThrow(() -> new RuntimeException("게시글이 존재하지 않습니다."));
+
+        GuestComment newGuestComment = GuestComment.builder()
+                .createdAt(LocalDateTime.now())
+                .guestName(createCommand.guestName())
+                .guestPassword(createCommand.guestPassword()).build();
+
+        newGuestComment.assignPost(post);
+        return guestCommentRepository.save(newGuestComment)
+                .getId();
     }
 }
