b0660d388442941218f8394ae428bbdd93165164
biddan606 <biddan606@gmail.com>
2024-10-24 01:09:43 +0900

    feat: 생성 BaseEntity 구현
    생성 시간과 업데이트 시간 저장

---
+++ src/main/java/com/nubble/backend/common/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.nubble.backend.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
