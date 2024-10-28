6e21d307afe1516733f204db56e8c1c35275e990
biddan606 <biddan606@gmail.com>
2024-10-28 21:23:32 +0900

    feat: Mapstruct 기본 설정 인터페이스 추가

---
+++ src/main/java/com/nubble/backend/common/BaseMapperConfig.java
@@ -0,0 +1,14 @@
+package com.nubble.backend.common;
+
+import org.mapstruct.InjectionStrategy;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(
+        componentModel = "spring",
+        injectionStrategy = InjectionStrategy.CONSTRUCTOR,
+        unmappedTargetPolicy = ReportingPolicy.ERROR
+)
+public interface BaseMapperConfig {
+
+}
