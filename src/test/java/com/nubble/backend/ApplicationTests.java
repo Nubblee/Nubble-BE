61a7b222c5101722c868ed7f7371b7edac6037ce
biddan606 <biddan606@gmail.com>
2024-10-31 11:00:15 +0900

    test: 개발 시 필요하지 않은 테스트 disabled 처리

--- src/test/java/com/nubble/backend/ApplicationTests.java
+++ src/test/java/com/nubble/backend/ApplicationTests.java
@@ -1,8 +1,10 @@
 package com.nubble.backend;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled("애플리케이션 컨텍스트 로드 테스트 필요 시에만 실행")
 @SpringBootTest
 class ApplicationTests {
 
--- src/test/java/com/nubble/backend/config/AwsConfigTest.java
+++ src/test/java/com/nubble/backend/config/AwsConfigTest.java
@@ -2,6 +2,7 @@ package com.nubble.backend.config;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.Bucket;
 import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
 
+@Disabled("개발 중 S3 연결 테스트는 필요하지 않기 때문에 비활성화됨. 필요시 주석 제거 후 실행")
 @SpringBootTest
 class AwsConfigTest {
 
